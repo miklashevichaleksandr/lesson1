5421dd7a903ff033fa413703e8c72a04d8b1d206
Aleksandr Miklashevich <miklashevichaleksandr@gmail.com>
2018-02-17 22:35:04 +0300

    Small change

--- bot.py
+++ bot.py
@@ -23,6 +23,6 @@ def greet_user(bot, update):
 def talk_to_me(bot, update):
     user_text = update.message.text 
     print(user_text)
-    update.message.reply_text('Нет ' + user_text)
+    update.message.reply_text(user_text)
 
 main()
